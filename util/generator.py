5901b102cd09c90c06500ea96ffddc1c95936e86
funmi7 <talabifunmilayo@gmail.com>
2020-03-05 21:20:35 +0100

    Generated map with rooms

--- util/generator.py
+++
@@ -1,8 +0,0 @@
-from util.rooms import rooms_dict
-from adventure.models import Player, Room
-
-def generate_room():
-    for key, value in rooms_dict.items():
-        key = Room(title=value['title'], description=value['description'])
-        key.save()
-
